2508edd4d0ba7308198bca3eff058c2f1c0e579a
Bunnyhug01 <mkavkovv@gmail.com>
2023-04-13 23:04:06 +0300

    Add Django project

---
+++ OntologyApp/main/sparql_util.py
@@ -0,0 +1,12 @@
+from row_sparql_util import *
+
+
+def create_triple(subject, predicate, object):
+    return update('INSERT DATA {' + f'{subject} {predicate} {object}' + '}')
+
+
+m  = Node('http://www.w3.org/2023/03/31-my#maks')
+n  = Node('http://www.w3.org/2023/03/31-my#name')
+ml = Literal('Maksim')
+
+print(create_triple(m, n, ml))
